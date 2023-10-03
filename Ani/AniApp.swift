3b763fad4c707f5f18a00f480eed83a8d30a8e8c
Juliet Gobran <julietkate.g@gmail.com>
2023-10-03 11:06:53 +0800

    Initial Commit

---
+++ Ani/AniApp.swift
@@ -0,0 +1,17 @@
+//
+//  AniApp.swift
+//  Ani
+//
+//  Created by Juliet Gobran on 3/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct AniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
